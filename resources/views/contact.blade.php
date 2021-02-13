d99e956b1b969061c51251e0ef74a078c14c1811
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-02-13 11:59:17 +0700

    EDIT Welcome About Contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,9 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="/">HOME | </a>
-    <a href="about">ABOUT | </a>
-    <a href="contact">CONTACT | </a>
+    <a href="/">HOME</a> | <a href="about">ABOUT</a> | <a href="contact">CONTACT</a>
     <h1>CONTACT ME</h1>
 </body>
 </html>
