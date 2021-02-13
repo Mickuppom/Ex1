b0d45a83574a106d48a26fe2ea786f3b16eae2d8
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-02-13 14:28:25 +0700

    YIELD!!!

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/">HOME</a> | <a href="about">ABOUT</a> | <a href="contact">CONTACT</a>
-    <h1>ABOUT ME</h1>
+@extends('layout.master')
 
-</body>
-</html>
+@section('content')
+    <h1>ABOUT ME</h1>
+@endsection
