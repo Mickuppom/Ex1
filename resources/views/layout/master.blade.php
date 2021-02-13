5a077f3932df14582db9ff7be419f98738a67340
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-02-13 15:22:25 +0700

    Update Database

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>FUCKING WEB</title>
 </head>
 <body>
     <a href="/">HOME</a> | <a href="about">ABOUT</a> | <a href="contact">CONTACT</a>
